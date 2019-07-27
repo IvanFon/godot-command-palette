e46d419fa40d381397992b7c1b72c20f1352aeef
Ivan Fonseca <ivanfon@riseup.net>
2019-07-26 22:18:10 -0400

    Rename Finder to Palette

--- command_palette.gd
+++ command_palette.gd
@@ -1,15 +1,15 @@
 tool
 extends EditorPlugin
 
-const Finder = preload("res://addons/godot-command-palette/Finder.tscn")
+const Palette = preload("res://addons/godot-command-palette/Palette.tscn")
 const FileIndex = preload("res://addons/godot-command-palette/file_index.gd")
 
-# Instance of finder scene
-var finder: Node
-var finder_window: WindowDialog
-var finder_list: ItemList
-var finder_search: LineEdit
-# If finder is open
+# Instance of palette scene
+var palette: Node
+var palette_window: WindowDialog
+var palette_list: ItemList
+var palette_search: LineEdit
+# If palette is open
 var open := false
 # Input tracking
 var inputs := {
@@ -20,20 +20,20 @@ var inputs := {
 var cur_mode
 
 func _enter_tree() -> void:
-	# Create finder and add to editor
-	finder = Finder.instance() as Control
-	add_control_to_container(CONTAINER_TOOLBAR, finder)
+	# Create palette and add to editor
+	palette = Palette.instance() as Control
+	add_control_to_container(CONTAINER_TOOLBAR, palette)
 	
 	# Get controls
-	finder_window = finder.get_node("WindowDialog") as WindowDialog
-	finder_list = finder.get_node("WindowDialog/List") as ItemList
-	finder_search = finder.get_node("WindowDialog/Search") as LineEdit
+	palette_window = palette.get_node("WindowDialog") as WindowDialog
+	palette_list = palette.get_node("WindowDialog/List") as ItemList
+	palette_search = palette.get_node("WindowDialog/Search") as LineEdit
 	
 	# Connect signals
-	# Finder
-	finder_window.connect("popup_hide", self, "_on_finder_closed")
-	finder_search.connect("text_changed", self, "_on_search_text_changed")
-	finder_search.connect("text_entered", self, "_on_search_text_entered")
+	# Palette
+	palette_window.connect("popup_hide", self, "_on_palette_closed")
+	palette_search.connect("text_changed", self, "_on_search_text_changed")
+	palette_search.connect("text_entered", self, "_on_search_text_entered")
 	# Filesystem changes
 	get_editor_interface().get_resource_filesystem().connect("filesystem_changed", self, "_on_filesystem_changed")
 	
@@ -43,13 +43,13 @@ func _enter_tree() -> void:
 	cur_mode = preload("res://addons/godot-command-palette/modes/mode_files.gd").new(get_editor_interface(), file_index)
 
 func _exit_tree() -> void:
-	remove_control_from_container(CONTAINER_TOOLBAR, finder)
+	remove_control_from_container(CONTAINER_TOOLBAR, palette)
 
 func _process(delta: float) -> void:
 	# Keyboard shortcuts (closed)
 	if (Input.is_key_pressed(KEY_CONTROL) &&
 			Input.is_key_pressed(KEY_P)):
-		open_finder()
+		open_palette()
 	
 	# Keyboard shortcuts (open)
 	if !open: return
@@ -67,74 +67,74 @@ func _process(delta: float) -> void:
 	else:
 		inputs.down = true
 
-func open_finder() -> void:
+func open_palette() -> void:
 	if open: return
 	open = true
 	
 	# Show window
-	finder_window.popup_centered()
-	finder_search.grab_focus()
+	palette_window.popup_centered()
+	palette_search.grab_focus()
 	
 	cur_mode.palette_opened()
 	
 	# Populate file list
-	cur_mode.populate_list(finder_list)
+	cur_mode.populate_list(palette_list)
 
-func close_finder() -> void:
+func close_palette() -> void:
 	if !open: return
 	open = false
 	
 	# Hide window
-	finder_window.hide()
+	palette_window.hide()
 
-func _on_finder_closed() -> void:
+func _on_palette_closed() -> void:
 	if !open: return
 	open = false
 	
 	cur_mode.palette_closed()
 	
 	# Clear controls
-	finder_list.clear()
-	finder_search.clear()
+	palette_list.clear()
+	palette_search.clear()
 
 # Filter list when search text changed
 func _on_search_text_changed(text: String) -> void:
-	cur_mode.search_changed(text, finder_list)
+	cur_mode.search_changed(text, palette_list)
 
 # Open result
 func _on_search_text_entered(text: String) -> void:
 	# Only cleanup if mode triggers successfully (true)
-	if cur_mode.triggered(finder_list):
-		finder_search.clear()
-		close_finder()
+	if cur_mode.triggered(palette_list):
+		palette_search.clear()
+		close_palette()
 
 # Get selected item index in item list
 func get_selected_item_index() -> int:
-	return finder_list.get_selected_items()[0]
+	return palette_list.get_selected_items()[0]
 
 func move_selection_up() -> void:
-	if finder_list.get_item_count() == 0:
+	if palette_list.get_item_count() == 0:
 		return
 	
 	var cur_index = get_selected_item_index()
 	if cur_index - 1 < 0:
-		finder_list.select(finder_list.get_item_count() - 1)
+		palette_list.select(palette_list.get_item_count() - 1)
 	else:
-		finder_list.select(cur_index - 1)
+		palette_list.select(cur_index - 1)
 	
-	finder_list.ensure_current_is_visible()
+	palette_list.ensure_current_is_visible()
 
 func move_selection_down() -> void:
-	if finder_list.get_item_count() == 0:
+	if palette_list.get_item_count() == 0:
 		return
 	
 	var cur_index = get_selected_item_index()
-	if cur_index + 1 > finder_list.get_item_count() - 1:
-		finder_list.select(0)
+	if cur_index + 1 > palette_list.get_item_count() - 1:
+		palette_list.select(0)
 	else:
-		finder_list.select(cur_index + 1)
+		palette_list.select(cur_index + 1)
 	
-	finder_list.ensure_current_is_visible()
+	palette_list.ensure_current_is_visible()
 
 func _on_filesystem_changed():
 	cur_mode.filesystem_changed()
